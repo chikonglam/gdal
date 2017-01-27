f4556e3416923867aefbe69cc62b3f3fbb5db8a6
Bas Couwenberg <sebastic@xs4all.nl>
2017-01-27 12:49:38 +0100

    Strip pre-releases from ABI version when creating new symbols files.

--- debian/gdal-symbols.pl
+++ debian/gdal-symbols.pl
@@ -1139,6 +1139,7 @@ sub new_architecture_symbols {
 					my $upstream_version = upstream_version($versions[-1]);
 
 					(my $abi = $upstream_version) =~ s/\./-/g;
+					$abi =~ s/\~\w+$//;
 
 					my $i = 0;
 					foreach(read_file($filt)) {
